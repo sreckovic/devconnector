c0d563b05888807e0ab76103f38c085de7dcf77f
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-04-30 15:30:29 +0200

    Register validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -30,7 +30,7 @@ router.post('/register', (req, res) => {
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      erros.email = 'Email already exists';
+      errors.email = 'Email already exists';
       return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
