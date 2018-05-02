d68422a21f85ef84ca028a457a4bff4110050c3d
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-05-02 17:30:11 +0200

    validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load input validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load user model
 const User = require('../../models/User');
@@ -64,6 +65,13 @@ router.post('/register', (req, res) => {
 // @desc    Login user / Returning the JWT token
 // @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -71,7 +79,8 @@ router.post('/login', (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: 'User not found' });
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -100,7 +109,8 @@ router.post('/login', (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: 'Password incorrect' });
+        errors.passport = 'Password incorrect';
+        return res.status(400).json(errors);
       }
     });
   });
